802a282c5d3abdf9e2766cf907180bd69b74a997
awin6248 <awin6248@gmail.com>
2023-07-05 16:06:03 -0400

    changed CV page

--- src/Pages/Cv.js
+++ src/Pages/Cv.js
@@ -3,12 +3,41 @@ import './home.css';
 const CV = () => {
     return (
         <div className='main-content'>
-          <div className='cv-text'><small>Drawing artist and user interface designer based in Queens, New York City<br/>
-            <br/>Education<br/>2014 Masters - Information Management and Systems, University of California Berkeley<br/>2011 BA - Anthropology, University of California Los Angeles<br/>
-            <br/>Group Exhibitions<br/>
-            <div>2022&nbsp; &nbsp; Aggregate Space Gallery, Oakland<br/></div>2019 &nbsp;&nbsp; SSHH B.Y.O.A.S.S., NYC<br/>2016 &nbsp;&nbsp; Group Exhibition, The Galallery, San Francisco<br/>2016&nbsp; &nbsp; 2x2: Group Exhibition, The Galallery, San Francisco<br/><br/>Art Book and Zine Fairs<br/><div>2021&nbsp; &nbsp; NY Art Book Fair<br/></div>
-            2019&nbsp; &nbsp; LA Art Book Fair<br/>2018&nbsp; &nbsp; NY Tech Zine Fair, New York City<br/>2018&nbsp; &nbsp; 8-Ball Zine Fair, New York City<br/><br/>Organizations<br/>2016&nbsp; &nbsp; Adobe Books &amp; Arts Cooperative Volunteer, San Francisco</small><br/>
-            <div className='instagram-link'><small>IG: <a href="https://www.instagram.com/andrewkwin/" target="_blank"><span>@andrewkwin</span></a></small></div></div>
+          <div className='cv-text'><div>Drawing artist and user interface designer based in Queens, New York City</div>
+        <div className='cv-list'>
+          <br/>
+            <div className='cv-header'>Education</div><br/>
+            <div className='cv-item'><div className='year'>2014 Masters</div><div>Information Management and Systems <br></br>University of California Berkeley</div></div>
+            <div className='cv-item'><div className='year'>2011 BA</div><div> Anthropology, University of California Los Angeles</div></div>
+            <div className='cv-header'>Group Exhibitions</div>
+            <div className='cv-item'><div className='year'>2022</div><div>Aggregate Space Gallery, Oakland</div></div>
+            <div className='cv-item'><div className='year'>2019</div><div>SSHH B.Y.O.A.S.S., NYC</div></div>
+            <div className='cv-item'><div className='year'>2016</div><div>Group Exhibition, The Galallery, San Francisco</div></div>
+            <div className='cv-item'><div className='year'>2016</div><div>2x2: Group Exhibition, The Galallery, San Francisco</div></div>
+            <div className='cv-header'>Art Book and Zine Fairs<br/></div>
+            <div className='cv-item'>
+                <div className='year'>2021</div>
+                <div>NY Art Book Fair<br/></div>
+            </div>
+            <div className='cv-item'>
+                <div className='year'>2019</div>
+                <div>LA Art Book Fair<br/></div>
+            </div>
+
+            <div className='cv-item'>
+                <div className='year'>2018</div>
+                <div>NY Tech Zine Fair, New York City</div>
+            </div>
+            <div className='cv-item'>
+                <div className='year'>2018</div>
+                <div>8-Ball Zine Fair, New York City Organizations</div>
+            </div>
+            <div className='cv-item'>
+                <div className='year'>2016</div>
+                <div>Adobe Books &amp; Arts Cooperative Volunteer, San Francisco</div>
+            </div>
+        </div>    
+        <div className='instagram-link'>IG: <a href="https://www.instagram.com/andrewkwin/" target="_blank"><span>@andrewkwin</span></a></div></div>
       </div>
     )   
 };
